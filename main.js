e49dbb4992225002ea2429f805eb6bacce5a3dc8
Mariana Lima <mariana.lima118@outlook.com>
2023-02-04 09:12:54 -0300

    add effects audio

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
-const btnMode = document.querySelector(".btn-mode")
-const iconMode = document.querySelector(".icon-mode")
+const btnMode = document.querySelector(".btn-mode");
+const iconMode = document.querySelector(".icon-mode");
+const btnEffect = document.querySelectorAll('input[type="radio"]');
 let theme = false;
+var audio = new Audio();
 
-function LightDarkTheme(){
-  if(theme){
-    iconMode.setAttribute("src", "assets/images/moon.svg")
-    document.querySelector("body").style.background = '#29292E'
-  } else {
-    iconMode.setAttribute("src", "assets/images/sun.svg")
-    document.querySelector("body").style.background = '#e5e5e5'
+function audioPlay(value){
+  audio.src = `./assets/sound/${value}.wav`
+  audio.play()
+  audio.loop = true;
+}
+
+function verifyEffect(value){
+  switch(value.id){
+    case 'rain':
+      audioPlay(value.id)
+      break
+    case 'forest':
+      audioPlay(value.id)
+      break
+    case 'coffee':
+      audioPlay(value.id)
+      break
+    case 'fire':
+      audioPlay(value.id)
+      break
   }
 }
 
-btnMode.addEventListener('click',() => {
-  theme == true ? theme = false : theme = true
-  LightDarkTheme()
-})
+btnEffect.forEach((card) => {
+  card.addEventListener('change', () => {
+    if (card.checked == true) {
+      verifyEffect(card)
+    }
+  })
+});
+
+function LightDarkTheme() {
+  if (theme) {
+    iconMode.setAttribute("src", "assets/images/moon.svg");
+    document.querySelector("body").style.background = "#29292E";
+  } else {
+    iconMode.setAttribute("src", "assets/images/sun.svg");
+    document.querySelector("body").style.background = "#e5e5e5";
+  }
+}
+
+btnMode.addEventListener("click", () => {
+  theme == true ? (theme = false) : (theme = true);
+  LightDarkTheme();
+});
